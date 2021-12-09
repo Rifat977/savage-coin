9457800c04ff340bad74938d38157586b88cf62f
Developer Rifat <itscrifat5147@gmail.com>
2021-12-09 11:07:17 +0600

    second block

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 const SHA256 = require('crypto-js/sha256');
 
 class Block {
-    constructor(index, timestamp, data, previousHash = '') {
+    constructor(index, timestamp, data, previousHash) {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.hash = this.calculateHash();
     }
-
     calculateHash() {
         return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString()
     }
@@ -20,7 +18,7 @@ class Blockchain {
     }
 
     createGenesisBlock() {
-        return new Block(0, "01/01/2021", "First Genesis Block", "0");
+        return new Block(0, "01/01/2021", "First Block", null);
     }
 
     getLatestBlock() {
@@ -32,10 +30,30 @@ class Blockchain {
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 let savageCoin = new Blockchain();
-savageCoin.addBlock(new Block(1, "10/02/2021", { amount: 4 }));
-savageCoin.addBlock(new Block(1, "10/01/2021", { amount: 10 }));
+savageCoin.addBlock(new Block(1, "09/01/2021", { amount: 400 }));
+savageCoin.addBlock(new Block(2, "10/01/2021", { amount: 100 }));
+savageCoin.addBlock(new Block(3, "11/01/2021", { amount: 300 }));
 
-console.log(JSON.stringify(savageCoin, null, 4));
+
+// console.log(JSON.stringify(savageCoin, null, 4));
+console.log('Is valid -> ' + savageCoin.isChainValid());
+savageCoin.chain[1].data = { amount: 600 }
+console.log('Is valid -> ' + savageCoin.isChainValid());
